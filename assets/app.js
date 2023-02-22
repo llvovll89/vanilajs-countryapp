353c448b78328700cf72443c07b26e5d4bd808ae
llvovll89 <svvvs5579@naver.com>
2023-02-23 02:24:20 +0900

    mobile size update + timer & date add

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,48 @@
+// dom
 const wrap = document.querySelector(".wrap"),
   container = document.querySelector(".container"),
   list = document.querySelector(".list"),
   countryName = document.querySelector("input"),
   body = document.querySelector("#body"),
-  textbox = document.querySelector(".textbox p");
+  textbox = document.querySelector(".textbox p"),
+  timer = document.querySelector(".timer"),
+  todayDate = document.querySelector(".date");
 
+window.onload = () => {
+  clock();
+  setInterval(clock, 1000);
+};
+
+// Time (textbox에)
+function clock() {
+  const newDate = new Date();
+  const hours = modifyNum(newDate.getHours());
+  const minit = modifyNum(newDate.getMinutes());
+  const sec = modifyNum(newDate.getSeconds());
+  const ampm = hours > 12 ? "PM" : "AM";
+
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const date = newDate.getDate();
+
+  timer.innerHTML = `${hours}:${minit}:${sec}<span class="ampm">(${ampm})</span>`;
+  todayDate.innerHTML = `${year}년 ${month}월 ${date}일`;
+}
+
+function modifyNum(time) {
+  if (parseInt(time) < 10) {
+    return `0${time}`;
+  } else {
+    return time;
+  }
+}
+
+// 초기화
 const nArr = [];
 countryName.focus();
 textbox.innerText = "";
 
+// form 이벤트
 const form = document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
   container.classList.add("active");
@@ -18,7 +52,6 @@ const form = document.getElementById("form").addEventListener("submit", (e) => {
   const val = countryName.value;
   getAPI(val);
   pushArr(val);
-  bgFor(100);
 });
 
 // random
@@ -36,19 +69,16 @@ const createEffect = () => {
 };
 
 const bgFor = (num) => {
-  for (let i = 0; i < num; i++) {
-    createEffect(num[i]);
+  if (num !== parseInt(0)) {
+    for (let i = 0; i < num; i++) {
+      createEffect();
+    }
+  } else {
+    return num;
   }
 };
 
-// function onlyKR(e) {
-//   const pattern = /^[가-힣\s]+$/;
-//   console.log(this.value);
-// }
-
-// 한글만입력 key
-// countryName.addEventListener("keypress", onlyKR);
-
+// text박스 배열값넣기
 const pushArr = (item) => {
   if (item.innerText === "") {
     return (textbox.innerText = null);
@@ -58,6 +88,7 @@ const pushArr = (item) => {
   return (textbox.innerText = nArr);
 };
 
+// 공공데이터 api 불러오기
 const getAPI = async (item) => {
   // API_KEY 숨기기,,,,
   const API_KEY =
@@ -81,13 +112,14 @@ const getAPI = async (item) => {
             <p><span>국가명</span> : ${item.country_nm}</p>
             <p><span>국가코드</span> : ${item.country_iso_alp2}</p>
             <p><span>영문명</span> : ${item.country_eng_nm}</p>
-            <p><span>동포</span> : ${item.oks_status}</p>
             <p><span>투자</span> : ${item.investment_status}</p>
+            <p><span>동포</span> : ${item.oks_status}</p>
             <p><span>수교일</span> : ${item.diplomatic_relations}</p>
                <button class="delbtn" onClick=remove() >❌</button>
                </li>
             `;
         countryName.value = "";
+        bgFor(300);
       });
     }, 3000);
   } else {
@@ -95,9 +127,11 @@ const getAPI = async (item) => {
     countryName.value = "";
     countryName.focus();
     remove();
+    bgFor(0);
   }
 };
 
+// 전체삭제 (active)
 function remove() {
   list.innerHTML = "";
   container.classList.remove("active");
